3f8f0d6b59dd98897244509cbd6d50b19da8dd93
Marwan Mohamed <marwan.mohamed7287@gmail.com>
2025-06-26 14:56:38 +0300

    Initial commit

---
+++ lib/Common/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
+      );
+    });
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/noteable_bg.png',
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
